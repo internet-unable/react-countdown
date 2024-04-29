1069b232072a7c60195e362fbc5a89255379b3b4
internet-unable <polocatic@gmail.com>
2024-04-29 22:43:58 +0300

    Updated ResultModal
    Learned how to use Portal

--- src/components/ResultModal/ResultModal.jsx
+++ src/components/ResultModal/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useRef, forwardRef, useImperativeHandle } from 'react';
+import { createPortal } from 'react-dom';
 
 const ResultModal = forwardRef(({ targetTime, remainingTime, onReset }, ref) => {
     const dialog = useRef();
@@ -14,7 +15,7 @@ const ResultModal = forwardRef(({ targetTime, remainingTime, onReset }, ref) => 
         }
     });
 
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal" onClose={onReset}>
             {userLost && <h2>You lost</h2>}
             {!userLost && <h2>Your score {score}</h2>}
@@ -23,7 +24,8 @@ const ResultModal = forwardRef(({ targetTime, remainingTime, onReset }, ref) => 
             <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
     );
 });
 
