ffc2a332772832b3646156d097e8ff1fb797b2c8
internet-unable <polocatic@gmail.com>
2024-04-29 19:15:14 +0300

    Update for ResultModal and TimerChallenge
    Updated main components to provide score system and some other small edits.

--- src/components/TimerChallenge/TimerChallenge.jsx
+++ src/components/TimerChallenge/TimerChallenge.jsx
@@ -4,37 +4,48 @@ import ResultModal from "../ResultModal/ResultModal.jsx";
 export default function TimerChallenge({ title, targetTime }) {
     const timer = useRef();
     const dialog = useRef();
-    const [timerStarted, setTimerStarted] = useState(false);
-    // const [timerExpired, setTimerExpired] = useState(false);
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+    if (timeRemaining <= 0) {
+        clearInterval(timer.current);
+        dialog.current.open();
+    }
+    
+    function handleReset() {
+        setTimeRemaining(targetTime * 1000);
+    }
 
     function handleBtnClick() {
-        if (timerStarted) {
-            clearTimeout(timer.current);
-            setTimerStarted(false);
+        if (timerIsActive) {
+            clearInterval(timer.current);
+            dialog.current.open();
         } else {
-            timer.current = setTimeout(() => {
-                // setTimerExpired(true);
-                dialog.current.open();
-            }, targetTime * 1000);
-
-            setTimerStarted(true);
+            timer.current = setInterval(() => {
+                setTimeRemaining(prevTimeRemainig => prevTimeRemainig - 10);
+            }, 10);
         }
     }
 
     return (
         <>
-            <ResultModal ref={dialog} result="lost" targetTime={targetTime} />
+            <ResultModal
+                ref={dialog}
+                targetTime={targetTime}
+                remainingTime={timeRemaining}
+                onReset={handleReset}
+            />
 
             <section className="challenge">
                 <h2>{title}</h2>
                 <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : ''}</p>
                 <p>
                     <button type="button" onClick={handleBtnClick}>
-                        {timerStarted ? 'Stop' : 'Start'} Challange
+                        {timerIsActive ? 'Stop' : 'Start'} Challange
                     </button>
                 </p>
-                <p className={timerStarted ? 'active' : undefined}>
-                    {timerStarted ? 'Time is running...' : 'Timer inactive'}
+                <p className={timerIsActive ? 'active' : undefined}>
+                    {timerIsActive ? 'Time is running...' : 'Timer inactive'}
                 </p>
             </section>
         </>
